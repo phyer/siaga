a86c5fc4cfbb239d54d423cfe6fe4aa8a0138ec8
zhangkun9038@dingtalk.com <zhangkun9038@dingtalk.com>
2024-12-17 10:52:02 +0800

    只收到一条消息问题的修复

--- modules/extent.go
+++ modules/extent.go
@@ -215,6 +215,7 @@ func GetRangeCandleSortedSet(cr *core.Core, setName string, count int, from time
 		logrus.Warning("移出过期的引用数量：", setName, count, "ZRemRangeByScore ", setName, 0, strconv.FormatInt(oti, 10))
 	}
 	logrus.Info("ZRevRangeByScore ", setName, opt)
+	fmt.Println("any: ", any)
 	ary, err = cli.ZRevRangeByScore(setName, opt).Result()
 	if err != nil {
 		return &cdl, err
