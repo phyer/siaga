fbe02ab891de424a71f1f4fe0a4f0eea1fb27c44
zhangkun9038@dingtalk.com <zhangkun9038@dingtalk.com>
2024-12-19 22:18:52 +0800

    logrus info

--- modules/extent.go
+++ modules/extent.go
@@ -227,7 +227,6 @@ func GetRangeCandleSortedSet(cr *core.Core, setName string, count int, from time
 		return &cdl, err
 	}
 	for _, str := range keyAry {
-		fmt.Println("str1:", str)
 		if str == nil {
 			continue
 		}
