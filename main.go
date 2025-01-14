5cf9cf17c825050b685bd0c719344bf4c0039401
zhangkun9038@dingtalk.com <zhangkun9038@dingtalk.com>
2025-01-14 22:11:29 +0800

    tracebug of insertPlate

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	cr := core.Core{}
 	cr.Init()
-	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetLevel(logrus.WarnLevel)
 	cr.TickerInforocessChan = make(chan *core.TickerInfo)
 	cr.CandlesProcessChan = make(chan *core.Candle)
 	cr.MaXProcessChan = make(chan *core.MaX)
